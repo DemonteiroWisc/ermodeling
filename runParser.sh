cbd8ae85e55908c05c02cd288b4cbad1e945ba8c
Daniel de Monteiro <demonteiro@wisc.edu>
2022-04-20 15:33:12 -0500

    updated runParser.sh to sort and uniq .dat files

--- runParser.sh
+++ runParser.sh
@@ -1,2 +1,15 @@
+sed -i 's+\r++' runParser.sh
 rm -f -- item.dat bid.dat user.dat cat.dat
 python3 my_parser.py ebay_data/items-*.json
+cat user.dat | sort | uniq > result
+rm -f user.dat
+mv result user.dat
+cat cat.dat | sort | uniq > result
+rm -f cat.dat
+mv result cat.dat
+cat bid.dat | sort | uniq > result
+rm -f bid.dat
+mv result bid.dat
+cat item.dat | sort | uniq > result
+rm -f item.dat
+mv result item.dat
